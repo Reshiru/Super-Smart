73b5ca4722442606aa134713859bddd68fd3375c
Stefano Palazzo <steff.palazzo@hotmail.com>
2018-06-08 14:36:24 +0200

    Implement delete document method

---
+++ src/SuperSmart/SuperSmart.Core/Persistence/Implementation/DocumentPersistence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SuperSmart.Core.Data.Connection;
+using SuperSmart.Core.Data.Implementation;
+using SuperSmart.Core.Data.ViewModels;
+using SuperSmart.Core.Extension;
+using SuperSmart.Core.Persistence.Interface;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SuperSmart.Core.Persistence.Implementation
+{
+    class DocumentPersistence : IDocumentPersistence
+    {
+        public void Delete(int id, string loginToken)
+        {
+            using (var db = new SuperSmartDb())
+            {
+                var account = db.Accounts.SingleOrDefault(a => a.LoginToken == loginToken);
+
+                if (account == null)
+                {
+                    throw new PropertyExceptionCollection(nameof(loginToken), "Your account couldn't be found. Pleas try to relogin");
+                }
+
+                var document = db.Documents.SingleOrDefault(a => a.Id == id);
+
+                if (document == null)
+                {
+                    throw new PropertyExceptionCollection(nameof(document), "The given document couldn't be found");
+                }
+
+                if (document.Task.Subject.TeachingClass.Admin != account)
+                {
+                    throw new PropertyExceptionCollection(nameof(account), "You are not permitted to this changes");
+                }
+
+                document.Active = false;
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
